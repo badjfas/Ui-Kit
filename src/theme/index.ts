6966fcb58e75b0b272346849e819e5b8a372d4a0
배진우 <bjwkor@naver.com>
2022-01-15 18:10:59 +0900

    init project config

---
+++ src/theme/index.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Noto Sans KR", "sans-serif"].join(", "),
+  },
+});
+
+export default theme;
